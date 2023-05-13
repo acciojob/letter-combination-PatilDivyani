acffa4917007a2ac622721f149b29ecccff7ba0b
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-13 20:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let map = [ 0 , 1 , abc, def, ghi, jkl, mno, pqrs, tuv, wxyz];
+	let output = [];
+	for (let i = 0; i < input_digit.length; i++) {
+		let digit = map[input_digit[i]];
+		let combo = [];
+
+		//outer loop form entire array
+		for (let j = 0; j < output.length; j++) { 
+			//to form combo 
+			for (let k = 0; k <  digit.length; k++) {
+				combo.push(output[j] + digit[k]);
+			}
+		}
+		output = combo;
+	}
+	return output;
 }
 
 module.exports = letterCombinations;
